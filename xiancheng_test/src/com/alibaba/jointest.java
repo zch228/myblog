071a27085156b8c613d86df8aa8dd2b9662270f7
zch228 <916215424@qq.com>
2020-03-19 15:03:26 +0800

    new repositories

---
+++ xiancheng_test/src/com/alibaba/jointest.java
@@ -0,0 +1,39 @@
+package com.alibaba;
+
+public class jointest {
+//1.现在有T1、T2、T3三个线程，你怎样保证T2在T1执行完后执行，T3在T2执行完后执行
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("t1");
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    t1.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("t2");
+            }
+        });
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    t2.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("t3");
+            }
+        });
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+}
